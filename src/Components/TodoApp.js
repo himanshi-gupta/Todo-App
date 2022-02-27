76f05e4ba99920f0e6b6dc6c67f124931b91b0ce
himanshi-gupta <himanshigupta579@gmail.com>
2022-02-27 14:00:14 +0530

    Added Update feature

--- src/Components/TodoApp.js
+++ src/Components/TodoApp.js
@@ -1,4 +1,4 @@
-import React,{useState,useRef,useEffect} from 'react'
+import React,{useState,useEffect} from 'react'
 import {MdDelete, MdOutlineDoneOutline, MdOutlineErrorOutline} from 'react-icons/md'
 import {FaEdit} from 'react-icons/fa'
 import checklist_homepage from '../checklist_homepage.png'
@@ -15,31 +15,55 @@ const getLocalList = () =>{
 
 function TodoApp(){
     var [todoList,setTodoList]=useState(getLocalList())
-    const [currentTask,setCurrentTask]=useState('')
-    const inputTask=useRef(null)
+    const [currentTask,setCurrentTask]=useState('');
+    const [toggleEdit,setToggleEdit]=useState(true);
+    const [isEditTask,setIsEditTask]=useState(null);
 
     const AddTask=()=>{
-        setTodoList([...todoList,{title:currentTask,completed:false}])
-        inputTask.current.value="";
-        setCurrentTask("");
+        if(!currentTask)
+            alert("Please add task!")
+        else if(currentTask && !toggleEdit){
+            setTodoList(
+                todoList.map((task)=>{
+                    if(task.id===isEditTask){
+                        return {...task,title:currentTask}
+                    }
+                    return task;
+                })
+            )
+            setToggleEdit(true);
+            setCurrentTask('')
+        }
+        else{
+            setTodoList([...todoList,{id : new Date().getTime().toString(),title:currentTask,completed:false}])
+            setCurrentTask('');
+        }
     };
 
-    const ClearAllTask=()=>{
+    const clearAllTask=()=>{
         setTodoList([])
-        inputTask.current.value="";
-        setCurrentTask("");
+        setCurrentTask('');
     }
 
-    const deleteTask=(taskToDelete)=>{
+    const editTask=(taskToEditId)=>{
+        let editItem=todoList.find((task) => {
+            return taskToEditId===task.id
+        })
+        setToggleEdit(false);
+        setCurrentTask(editItem.title)
+        setIsEditTask(taskToEditId);
+    }
+
+    const deleteTask=(taskToDeleteID)=>{
         setTodoList(todoList.filter((task)=>{
-            return task.title!==taskToDelete.title;
+            return taskToDeleteID!==task.id;
         }))
     };
 
-    const completeTask=(taskToComplete)=>{
+    const completeTask=(taskToCompleteID)=>{
         setTodoList(todoList.map((task)=>{
-            return (taskToComplete.title===task.title ? {title:taskToComplete.title,completed:true} 
-            : {title:task.title, completed:task.completed ? true :false }
+            return (taskToCompleteID===task.id ? {...task,completed:!task.completed} 
+            : {...task}
             ) }))
     }
 
@@ -54,23 +78,23 @@ function TodoApp(){
             <div> 
             <p/><img src={checklist_homepage} width={55} height={70} alt="Logo" />
             <h1>Todo-App</h1>
-                <input type="text" onKeyDown={(event)=>{if(event.keyCode===13) AddTask()}} placeholder="Task.." ref={inputTask} onChange={(event)=>setCurrentTask(event.target.value)}/>
-                <button onClick={AddTask}>Add Task</button>
-                <button onClick={ClearAllTask}>Clear All Tasks</button>
+                <input type="text" onKeyDown={(event)=>{if(event.keyCode===13) AddTask()}} placeholder="Add your task..." value={currentTask} onChange={(event)=>setCurrentTask(event.target.value)}/>
+                {toggleEdit ? <button onClick={AddTask}>Add Task</button> : <button onClick={AddTask}>Save Task</button> }
+                <button onClick={clearAllTask}>Clear All Tasks</button>
             </div>
             <hr/>
             <div>
                 <ul>
-                {todoList.map((task,key)=>{
+                {todoList.map((task)=>{
                     return ( 
                     <div id="task">
-                        <li key={key}>{task.title}</li>
+                        <li key={task.id}>{task.title}</li>
                         &nbsp; &nbsp;
-                        <FaEdit color='green' size={30}/>
+                        <FaEdit onClick={()=>editTask(task.id)} color='green' size={30}/>
                         &nbsp; &nbsp;
-                        <MdDelete onClick={()=>deleteTask(task)} size={30}/>
+                        <MdDelete onClick={()=>deleteTask(task.id)} size={30}/>
                         &nbsp; &nbsp;
-                        {task.completed ? <h2><MdOutlineDoneOutline color='green' size={30}/></h2> : <h2><MdOutlineErrorOutline color='red' size={30} onClick={()=>completeTask(task)}/></h2>}
+                        {task.completed ? <h2><MdOutlineDoneOutline color='green' size={30} onClick={()=>completeTask(task.id)} /></h2> : <h2><MdOutlineErrorOutline color='red' size={30} onClick={()=>completeTask(task.id)}/></h2>}
                     </div>)
 
                 })}
